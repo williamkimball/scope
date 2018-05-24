46e7c194b5b6e06815f11cc1d1d1135457ee4890
William Kimball <wkkimball043@gmail.com>
2018-05-24 08:55:17 -0500

    fixed cookie for loop

---
+++ main.js
@@ -0,0 +1,7 @@
+const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+
+
+for (let x = 0; x < cookies.length; x++) {
+    const currentCookie = cookies[x]
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+}
