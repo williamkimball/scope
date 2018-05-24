d0b5f66dd36c0786c988ec9a6f71ea9e3be7ee00
William Kimball <wkkimball043@gmail.com>
2018-05-24 10:53:58 -0500

    added function names as comments

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
+//C is for Cookie
 // const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
-
 
 // for (let x = 0; x < cookies.length; x++) {
 //     const currentCookie = cookies[x]
 //     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
 // }
 
+//Conjunction Function
 // const conjunction = function (firstWord, secondWord) {
 //     const conjoinedWord = `${firstWord} ${secondWord}`
 //     console.log(conjoinedWord)
@@ -13,7 +14,7 @@
 
 // conjunction("Master", "Card")
 
-
+//Mod Squad
 //     const ModSquad = {
 //         "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
 //         "series": {
@@ -31,19 +32,36 @@
 
 // document.querySelector(".show-info").innerHTML = HTMLRepresentation
 
-const locations = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3]]
+//Simon Says
+// const locations = [[1, 1], [1, 2], [1, 3], [2, 1], [2, 2], [2, 3]]
 
-for (let k = 0; k < locations.length; k++) {
-    let invalidLocation = false
+// for (let k = 0; k < locations.length; k++) {
+//     let invalidLocation = false
 
-    if (k > 2) {
-        invalidLocation = true
-        if (invalidLocation == true) {
-            console.log("This location is invalid")
-        }
-    } else {
-        console.log(`There were ${k + 1} locations displayed`)
+//     if (k > 2) {
+//         invalidLocation = true
+//         if (invalidLocation == true) {
+//             console.log("This location is invalid")
+//         }
+//     } else {
+//         console.log(`There were ${k + 1} locations displayed`)
+//     }
+// }
+
+//Lambda Llama
+const llamaNamer = function () {
+    const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
+    const randomizer = Math.floor(Math.random() * 7)
+
+    const namer = function () {
+        const suffix = " the Llama"
+        const name = possibleNames[randomizer]
     }
+
+    return name + suffix
 }
+
+nameMaker = llamaNamer()
+console.log(nameMaker())
 
 
