98a58d31b3ee8cd3b893d6eb8c9248d15c9ed46a
William Kimball <wkkimball043@gmail.com>
2018-05-24 09:05:10 -0500

    fixed modsquad

--- main.js
+++ main.js
@@ -6,9 +6,27 @@
 //     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
 // }
 
-const conjunction = function (firstWord, secondWord) {
-    const conjoinedWord = `${firstWord} ${secondWord}`
-    console.log(conjoinedWord)
-}
+// const conjunction = function (firstWord, secondWord) {
+//     const conjoinedWord = `${firstWord} ${secondWord}`
+//     console.log(conjoinedWord)
+// }
 
-conjunction("Master", "Card")
+// conjunction("Master", "Card")
+
+
+    const ModSquad = {
+        "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+        "series": {
+            "start": "1968",
+            "end": "1973"
+        }
+    };
+
+    let HTMLRepresentation = `<h1>The Mod Squad</h1>`
+
+    ModSquad.members.forEach(member => {
+        HTMLRepresentation += `<div>${member}</div>`;
+    })
+
+
+document.querySelector(".show-info").innerHTML = HTMLRepresentation
